6d13d28158b647170f819c6a5e75b5f02d2e3a0e
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-07-02 09:24:28 +0800

    feat: add update btn (UI) in all quizzes and add css component btn
    - Add secondary style btn under css component btn.
    - Add update button in all quiz(home page).

--- simpleQuiz/src/js/components/allQuizzes.js
+++ simpleQuiz/src/js/components/allQuizzes.js
@@ -9,7 +9,7 @@ export const renderAllQuizzes = () => {
   if(totalQuizzes.length === 0) {
     const text = document.createElement('p');
     text.textContent = "NO QUIZZES AVAILABLE"
-    text.className = "mt-10 text-lg italic font-bold text-center text-gray-400";
+    text.classList.add("mt-10", "text-lg" ,"italic", "font-bold", "text-center", "text-gray-400");
     container.appendChild(text);
   }
   // use fragment to repaint all at once instead per each item
@@ -21,39 +21,50 @@ export const renderAllQuizzes = () => {
 
 export const createCard = (quiz) => {
   const card = document.createElement('div');
-  card.className = "rounded-md shadow-lg";
+  card.classList.add("rounded-md", "shadow-lg");
   card.setAttribute('data-id', quiz.id);
 
   // card img
   const img = document.createElement('img');
   img.src = "https://tse1.mm.bing.net/th?id=OIP.7JSN9trMauRbIA2Uiue-SwHaEK&pid=Api&P=0&h=220";
-  img.className = "w-full rounded-t-md";
+  img.classList.add("w-full", "rounded-t-md");
   img.alt = "Quiz time picture";
 
   // card body
   const cardBody = document.createElement('div');
-  cardBody.className = "grid gap-2 p-2";
+  cardBody.classList.add("grid", "gap-2", "p-2");
 
   // card title
   const title = document.createElement('h2');
-  title.className = "mt-4 text-base font-medium sm:text-lg";
+  title.classList.add("mt-4" ,"text-base", "font-medium", "sm:text-lg");
   title.textContent = quiz.title;
 
   // card description
   const description = document.createElement('p');
-  description.className = "mb-4 text-base";
+  description.classList.add("mb-4", "text-base");
   description.textContent = `This quiz is 1-${quiz.size} long.`;
 
-  // card btn
-  const btn = document.createElement('a');
-  btn.href = `./takeQuiz.html?id=${quiz.id}`;
-  btn.className = "btn btn-primary"
-  btn.textContent = "Take Quiz"
+  // btn container
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('grid', 'gap-2', 'grid-cols-1', 'md:grid-cols-2');
 
-  cardBody.append(title, description, btn);
+  // card submit btn
+  const submit = document.createElement('a');
+  submit.href = `./takeQuiz.html?id=${quiz.id}`;
+  submit.classList.add("btn", "btn-primary");
+  submit.textContent = "Take Quiz";
+
+  // card update
+  const update = document.createElement('a');
+  update.href = `./updateQuiz.html?id=${quiz.id}`;
+  update.classList.add("btn", "btn-secondary");
+  update.textContent = "Update Quiz";
+
+  btnContainer.append(submit, update);
+  cardBody.append(title, description, btnContainer);
   card.append(img, cardBody);
   
   return card;
 }
 
-renderAllQuizzes()
+renderAllQuizzes();
